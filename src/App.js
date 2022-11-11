201797c22c700a82703e0c6d38bf3b69f47c637f
vanshika-singh518 <svanshika549@gmail.com>
2022-11-11 20:23:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { AboutUs } from "./components/AboutUs";
+import { Challenges } from "./components/Challenges";
+import { Speakers } from "./components/Speakers"
+import Timeline from "./components/Timeline"
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <AboutUs />
+      <Challenges />
+      <Speakers />
+      <Timeline />
+      <Footer />
     </div>
   );
 }
